30d7b1026e3538dc4b45e1c55141185ce51433df
madhuhema <madhunarmi@gmail.com>
2018-09-13 22:45:31 +0530

    issue fixed

--- js/carousel.js
+++ js/carousel.js
@@ -1,25 +1,47 @@
-var i;
-var slideIndex=1;
+var slideIndex = 1;
+var slideIndex2=0;
+showSlides2();
 showSlides(slideIndex);
-function plusSlides(n){
-    showSlides(slideIndex+=n);
+
+
+function plusSlides(n) {
+  showSlides(slideIndex += n);
 }
-function currentSlide(n){
-    showSlides(slideIndex=n);
+
+function currentSlide(n) {
+  showSlides(slideIndex = n);
 }
-function showSlides(n){
-    var slides=document.getElementsByClassName("slides");
-    var dots=document.getElementsByClassName("dot");
-    if(n>slides.length)
-      slideIndex=1;
-    else if(n<1)
-      slideIndex=slides.length;
-    for(i=0;i<slides.length;i++){
-        slides[i].style.display = "none";
-    }
-    for(i=0;i<dots.length;i++){
-        dots[i].className=dots[i].className.replace("active","");
-    }
-    slides[slideIndex-1].style.display="block";
-    dots[slideIndex-1].className+=" active";
+
+function showSlides(n) {
+  var i;
+  var slides = document.getElementsByClassName("slides");
+  var dots = document.getElementsByClassName("dot");
+  if (n > slides.length) {slideIndex = 1}    
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+      slides[i].style.display = "none";  
+  }
+  for (i = 0; i < dots.length; i++) {
+      dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";  
+  dots[slideIndex-1].className += " active";
+ 
+}
+function showSlides2(){
+var i;
+  var slides = document.getElementsByClassName("slides");
+  var dots = document.getElementsByClassName("dot");
+  for (i = 0; i < slides.length; i++) {
+      slides[i].style.display = "none";  
+  }
+  slideIndex2++;
+  if (slideIndex2> slides.length) {slideIndex2 = 1}    
+  if (slideIndex2 < 1) {slideIndex2 = slides.length}
+  for (i = 0; i < dots.length; i++) {
+      dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex2-1].style.display = "block";  
+  dots[slideIndex2-1].className += " active";
+  setTimeout(showSlides2, 4000);
 }
